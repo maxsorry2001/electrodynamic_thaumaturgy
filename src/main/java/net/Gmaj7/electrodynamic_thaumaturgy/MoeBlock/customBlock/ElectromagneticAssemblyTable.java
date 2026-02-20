89f51576c53bcc61533f80a2993c60611fbbf053
1=G <maxsorry2001@163.com>
2026-02-20 19:47:14 +0800

    材质重做

--- src/main/java/net/Gmaj7/electrodynamic_thaumaturgy/MoeBlock/customBlock/ElectromagneticAssemblyTable.java
+++ src/main/java/net/Gmaj7/electrodynamic_thaumaturgy/MoeBlock/customBlock/ElectromagneticAssemblyTable.java
@@ -9,13 +9,17 @@ import net.minecraft.world.MenuProvider;
 import net.minecraft.world.SimpleMenuProvider;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.inventory.ContainerLevelAccess;
+import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.phys.BlockHitResult;
+import net.minecraft.world.phys.shapes.CollisionContext;
+import net.minecraft.world.phys.shapes.VoxelShape;
 import org.jetbrains.annotations.Nullable;
 
 public class ElectromagneticAssemblyTable extends Block {
+    protected static final VoxelShape AABB = Block.box(0.0, 0.0, 0.0, 16.0, 10.0, 16.0);
     public ElectromagneticAssemblyTable(Properties properties) {
         super(properties);
     }
@@ -37,5 +41,10 @@ public class ElectromagneticAssemblyTable extends Block {
         return new SimpleMenuProvider((p1, p2, p3) -> {
             return new MoeAssemblyTableMenu(p1, p2, ContainerLevelAccess.create(level, pos));
         }, Component.translatable("block.electrodynamic_thaumaturgy.electromagnetic_assembly_table"));
+    }
+
+    @Override
+    protected VoxelShape getShape(BlockState state, BlockGetter level, BlockPos pos, CollisionContext context) {
+        return AABB;
     }
 }
